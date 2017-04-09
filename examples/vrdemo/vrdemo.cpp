3d85f3bc0d9030dacfd4c096b34051fec8258f5e
Florian Märkl <info@florianmaerkl.de>
2017-04-09 16:50:57 +0200

    Add Controllers to VR Demo

--- examples/vrdemo/vrdemo.cpp
+++ examples/vrdemo/vrdemo.cpp
@@ -1,4 +1,6 @@
 
+#include <openvr.h>
+
 #include <towerengine.h>
 #include <vr_context_openvr.h>
 
@@ -11,10 +13,12 @@ const int screen_height = 720;
 
 GLFWwindow *window = 0;
 
-tVRContext *vr_context = 0;
+tVRContextOpenVR *vr_context = 0;
 
 tWorld *world = 0;
 tScene *scene = 0;
+
+tMeshObject *controller_mesh_object[2];
 
 tVRForwardRenderer *renderer;
 
@@ -23,6 +27,7 @@ void InitGLFW();
 void InitEngine();
 void InitScene();
 void MainLoop();
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
 void Cleanup();
 
 int main()
@@ -41,7 +46,7 @@ void InitGLFW()
 		exit(1);
 
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	window = glfwCreateWindow(screen_width, screen_height, "Simple TowerEngine Demo", 0, 0);
+	window = glfwCreateWindow(screen_width, screen_height, "TowerEngine VR Demo", 0, 0);
 	if (!window)
 	{
 		Cleanup();
@@ -50,7 +55,11 @@ void InitGLFW()
 
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(0); // do not limit VR refresh rate to monitor
+
+	glfwSetKeyCallback(window, KeyCallback);
 }
+
+
 
 void InitEngine()
 {
@@ -89,15 +98,50 @@ void InitScene()
 		vr_context->GetEyeRenderHeight(tVRContext::RIGHT),
 		4,
 		world);
+
+	for (int i = 0; i<2; i++)
+	{
+		controller_mesh_object[i] = new tMeshObject(0);
+		world->AddObject(controller_mesh_object[i]);
+	}
 }
 
 void MainLoop()
 {
 	while (!glfwWindowShouldClose(window))
 	{
+		tVector origin = tVec(0.0f, 0.0f, 0.0f);
+
 		tVector center_pos;
 		tVector center_dir;
-		vr_context->StartFrame(tVec(0.0f, 0.0f), tVec(0.0f, 0.0f, 0.0f), center_pos, center_dir, renderer->GetCameras());
+		vr_context->StartFrame(tVec(0.0f, 0.0f), origin, center_pos, center_dir, renderer->GetCameras());
+
+		vr::TrackedDevicePose_t controller_poses[2];
+		vr::VRControllerState_t controller_states[2];
+		tMesh *controller_mesh[2];
+		vr_context->GetOpenVRControllerStates(origin, controller_poses, controller_states, controller_mesh);
+
+		for (int i = 0; i < 2; i++)
+		{
+			tMatrix4 mat = ConvertOpenVRMatrix(controller_poses[i].mDeviceToAbsoluteTracking);
+			float *mat_v = mat.GetData();
+
+			tMatrix3 basis = tMatrix3(tVec(mat_v[0], mat_v[1], mat_v[2]),
+				tVec(mat_v[4], mat_v[5], mat_v[6]),
+				tVec(mat_v[8], mat_v[9], mat_v[10]));
+
+			tVector pos = origin + tVec(mat_v[3], mat_v[7], mat_v[11]);
+
+			controller_mesh_object[i]->SetTransform(tTransform(basis, pos));
+			controller_mesh_object[i]->SetMesh(controller_mesh[i]);
+		}
+
+		//vr_last_buttons_pressed = vr_current_buttons_pressed;
+		//vr_current_buttons_pressed = controller_states[0].ulButtonPressed | controller_states[1].ulButtonPressed;
+
+		//show_crosshair = controller_states[0].rAxis[1].x > 0.1 || controller_states[1].rAxis[1].x > 0.1;
+
+
 		renderer->Render(vr_context->GetFBO());
 		vr_context->FinishFrame();
 
@@ -106,6 +150,15 @@ void MainLoop()
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
+	}
+}
+
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS)
+	{
+		if (key == GLFW_KEY_ESCAPE)
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
 }
 
